c630db1dac6a1a99fef494ecde18fe140905ee80
Leonardo Pardo <leonardoandrespardo@gmail.com>
2023-10-28 21:27:04 -0300

    slice sentiment examples

--- src/services/SentimentService.ts
+++ src/services/SentimentService.ts
@@ -1,53 +1,62 @@
 import { Sentiment } from "../models/Sentiment.mdb";
 import { MongoDBDatasource } from "../MyDataSoure";
 
-export class SentimentService{
+export class SentimentService {
+  private static sentimentRepository =
+    MongoDBDatasource.getRepository(Sentiment);
 
-    private static sentimentRepository = MongoDBDatasource.getRepository(Sentiment)
+  private static calculateAverageSentiment(data: Sentiment[]) {
+    let totalPositivity = 0;
+    let totalNegativity = 0;
+    let totalNeutrality = 0;
 
-    private static calculateAverageSentiment(data: Sentiment[]) {
-      let totalPositivity = 0;
-      let totalNegativity = 0;
-      let totalNeutrality = 0;
-  
-    
-      for (const item of data) {
-        totalPositivity += item.sentiment_positive;
-        totalNegativity += item.sentiment_negative;
-        totalNeutrality += item.sentiment_neutral;
-      }
-  
-      const numItems = data.length;
-      const averagePositivity = totalPositivity / numItems;
-      const averageNegativity = totalNegativity / numItems;
-      const averageNeutrality = totalNeutrality / numItems;
-    
-      if (averagePositivity > averageNegativity && averagePositivity > averageNeutrality){return "Positive"}
-      else if (averageNegativity > averagePositivity && averageNegativity > averageNeutrality){return "Negative"}
-      else {return "Neutral"}
-  
+    for (const item of data) {
+      totalPositivity += item.sentiment_positive;
+      totalNegativity += item.sentiment_negative;
+      totalNeutrality += item.sentiment_neutral;
     }
 
-    async getSentiment(): Promise<any> {
-        try {
-            const news = await SentimentService.sentimentRepository.find();
+    const numItems = data.length;
+    const averagePositivity = totalPositivity / numItems;
+    const averageNegativity = totalNegativity / numItems;
+    const averageNeutrality = totalNeutrality / numItems;
 
-            // Extraemos los URLs de las noticias
-            const examples = []
-            for (const obj of news){
-              examples.push(obj.url)
-            }
-        
-			// Calculamos el sentimiento en base a las noticias obtenidas
-			const result = await SentimentService.calculateAverageSentiment(news)
+    if (
+      averagePositivity > averageNegativity &&
+      averagePositivity > averageNeutrality
+    ) {
+      return "Positive";
+    } else if (
+      averageNegativity > averagePositivity &&
+      averageNegativity > averageNeutrality
+    ) {
+      return "Negative";
+    } else {
+      return "Neutral";
+    }
+  }
 
-			const static_object = {
-				result: result,
-				examples: examples
-			  }
-			return static_object
-        } catch (err) {
-          console.log(err);
-        }
+  async getSentiment(): Promise<any> {
+    try {
+      const news = await SentimentService.sentimentRepository.find();
+      const slice = news.slice(0, 5);
+
+      // Extraemos los URLs de las noticias
+      const examples = [];
+      for (const obj of slice) {
+        examples.push(obj.url);
       }
+
+      // Calculamos el sentimiento en base a las noticias obtenidas
+      const result = SentimentService.calculateAverageSentiment(news);
+
+      const static_object = {
+        result: result,
+        examples: examples,
+      };
+      return static_object;
+    } catch (err) {
+      console.log(err);
+    }
+  }
 }
